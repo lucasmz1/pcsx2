70439b7745b3b1bb477330f6c38d04d478dce52c
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-02-16 16:58:29 -0300

    Update file handling in appimage-qt-sharun.sh

--- .github/workflows/scripts/linux/appimage-qt-sharun.sh
+++ .github/workflows/scripts/linux/appimage-qt-sharun.sh
@@ -52,8 +52,9 @@ chmod +x ./sharun
 find ${GITHUB_WORKSPACE}/ -type f -iname 'pcsx2-qt' -executable | xargs -i -t -exec xvfb-run -- ./sharun l -p -v -e -k {}
 mkdir -p ./shared/lib/qt6 && find . -iname 'plugins' | xargs -i -t -exec mv {} ./shared/lib/qt6/
 find ./shared/lib/home/runner/deps/lib/ -iname 'lib*.so*' -print0 | xargs -0 -I{} cp -a {} ./shared/lib/
-find ./shared/lib/local/ -type f | xargs -i -t -exec mv {} ./shared/lib/
+find ./shared/lib/local/ -iname 'lib*.so*' -print0 | xargs -0 -I{} cp -a {} ./shared/lib/
 find . -iname 'home' | xargs -i -t -exec rm -rf {}
+find . -iname 'local' | xargs -i -t -exec rm -rf {}
 find "${GITHUB_WORKSPACE}" -iname 'qt.conf' -type f | xargs -i -t -exec cp {} "./bin/"
 ln ./sharun AppRun
 ./sharun -g
