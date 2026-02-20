fc2bd22c4adaf916a56a10777a92d0af7dc049f9
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-02-20 11:16:27 -0300

    Update appimage-qt-sharun.sh

--- .github/workflows/scripts/linux/appimage-qt-sharun.sh
+++ .github/workflows/scripts/linux/appimage-qt-sharun.sh
@@ -1,32 +1,5 @@
 #!/usr/bin/env bash
 
-# This is free and unencumbered software released into the public domain.
-#
-# Anyone is free to copy, modify, publish, use, compile, sell, or
-# distribute this software, either in source code form or as a compiled
-# binary, for any purpose, commercial or non-commercial, and by any
-# means.
-#
-# In jurisdictions that recognize copyright laws, the author or authors
-# of this software dedicate any and all copyright interest in the
-# software to the public domain. We make this dedication for the benefit
-# of the public at large and to the detriment of our heirs and
-# successors. We intend this dedication to be an overt act of
-# relinquishment in perpetuity of all present and future rights to this
-# software under copyright law.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
-#
-# For more information, please refer to <http://unlicense.org/>
-
-#!/usr/bin/env bash
-
 SCRIPTDIR=$(dirname "${BASH_SOURCE[0]}")
 
 set -e
@@ -69,7 +42,7 @@ find ${GITHUB_WORKSPACE}/ -type f -iname 'pcsx2-qt' -executable | xargs -i -t ./
 	/usr/lib/x86_64-linux-gnu/spa-0.2/*/* || true
 ./sharun l -p -v -e -s -k /usr/bin/vulkaninfo
 find /usr/lib/x86_64-linux-gnu/qt6/ -iname 'lib**' -print0 | xargs -0 -I{} ./sharun l -p -v -e -s -k {}
-mkdir -p ./shared/lib/qt6 && find . -iname 'plugins' | xargs -i -t -exec mv {} ./shared/lib/qt6/
+#mkdir -p ./shared/lib/qt6 && find . -iname 'plugins' | xargs -i -t -exec mv {} ./shared/lib/qt6/
 find ./shared/lib/home/runner/deps/lib/ -iname 'lib*.so*' -print0 | xargs -0 -I{} cp -a {} ./shared/lib/
 find ./shared/lib/local/ -iname 'lib*.so*' -print0 | xargs -0 -I{} cp -a {} ./shared/lib/
 find . -iname 'home' | xargs -i -t -exec rm -rf {}
